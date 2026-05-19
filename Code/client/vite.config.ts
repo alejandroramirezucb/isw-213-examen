cb8dcc702569e00b95c4db94a3134fe71f02978b
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2026-05-19 15:31:13 -0400

    refactor: actualizando estructura del codigo

--- Code/client/vite.config.ts
+++ Code/client/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   root: 'src/presentacion',
   build: {
-    outDir: '../../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
